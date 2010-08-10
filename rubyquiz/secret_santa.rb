54c9f7b0851d5faae76ca6981fcc88ff3e9fee39
unknown <Kumar.Gaurav@.internal.sungard.corp>
2010-08-10 16:54:55 +0530

    'quiz'

--- rubyquiz/secret_santa.rb
+++ rubyquiz/secret_santa.rb
@@ -1,10 +1,18 @@
 def fetch_list_of_names
+  namelist = []
   puts "Enter names in the following format: firstname lastname email\n"
-  namelist << gets until gets == 'n'
-
+  while true
+    if gets.chomp == 'stop'
+      break
+    else
+      namelist << gets.chomp
+    end
+  end
+  return namelist
 end
 
 def show_names(namelist)
+  namelist.size
   namelist.each do |name|
     puts name
   end
