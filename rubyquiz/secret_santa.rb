3fd1e1468cc0bcee4c86147e466dcea3fc6504c3
Kumar Gaurav <kgthegreat@gmail.com>
2010-08-11 08:34:41 +0530

    secret santa

--- rubyquiz/secret_santa.rb
+++ rubyquiz/secret_santa.rb
@@ -1,21 +1,34 @@
-def fetch_list_of_names
-  namelist = []
-  puts "Enter names in the following format: firstname lastname email\n"
-  while true
-    if gets.chomp == 'stop'
-      break
-    else
-      namelist << gets.chomp
-    end
+class Person
+  attr_reader :firstname,:lastname, :email
+  def initialize(person)
+    @firstname,@lastname,@email = person[0],person[1],person[2]
   end
-  return namelist
 end
 
-def show_names(namelist)
-  namelist.size
-  namelist.each do |name|
-    puts name
+def fetch_list_of_names
+  personlist = []
+  puts "Enter names in the following format: firstname lastname email\n"
+  while true
+    data = gets.chomp
+    if data == 'stop'
+      break
+    else
+     # puts "adding"+gets.chomp
+      personlist << Person.new(data.split(' '))
+    end
   end
+  return personlist.sort_by { rand }
+end
+
+def show_names(personlist)
+#  puts personlist.size
+  personlist.each do |person|
+    puts person.firstname
+  end
+end
+
+def randomize_array(array)
+
 end
 
 show_names(fetch_list_of_names)
